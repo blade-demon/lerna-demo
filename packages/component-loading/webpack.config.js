13e768f64243704ced47704d1a8bca5abd68b640
ziwei <xuziwei89@gmail.com>
2020-07-17 01:54:20 +0800

    fix bugs for component

--- packages/component-loading/webpack.config.js
+++ packages/component-loading/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,8 +8,8 @@ module.exports = {
     path: path.resolve(__dirname, "lib"),
     filename: "component-loading.js",
     libraryTarget: "umd",
-    library: ""
   },
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       {
@@ -23,15 +24,22 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
           },
         ],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: "svg-url-loader",
+          options: {},
+        },
       },
     ],
   },
